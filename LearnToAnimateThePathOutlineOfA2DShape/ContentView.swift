05084477c6b643c8253799f9aae231294f9cdf7c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-22 09:25:37 +0300

    init

--- LearnToAnimateThePathOutlineOfA2DShape/ContentView.swift
+++ LearnToAnimateThePathOutlineOfA2DShape/ContentView.swift
@@ -9,8 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //Initial State of Animation
+    @State var grow = false
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Today 22 September 2019")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Divider()
+            Spacer()
+            ZStack {
+                Circle()
+                    .trim(from: 0, to: 1)
+                    .stroke(Color.blue, lineWidth: 20)
+                    .frame(width: 300, height: 300)
+                    .opacity(0.2)
+                Circle()
+                    .trim(from: grow ? 1/9 : 1, to: 1)
+                    .stroke(Color.blue, lineWidth: 20)
+                    .frame(width: 300, height: 300)
+                    .rotationEffect(.degrees(90), anchor: .center)
+                    .animation(Animation.easeIn(duration: 2.5).repeatForever(autoreverses: false).delay(2.5))
+                    .onAppear() {
+                        self.grow.toggle()
+                }
+                VStack {
+                    Text("5.59 km")
+                        .font(.body)
+                        .fontWeight(.bold)
+                    Text("Step: 9332")
+                        .fontWeight(.semibold)
+                    Text("Goal 1000")
+                }
+            }
+        }
     }
 }
 
